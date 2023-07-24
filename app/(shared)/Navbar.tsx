ca8a66ab38f8004ffdf3a27afb3007ac598c9dde
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-23 22:09:56 -0400

    Started to implement Navbar

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div>Navbar</div>
+    <header className='mb-5'>
+        <nav className='flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4'>nav</nav>
+    </header>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import Navbar from 'app/(shared)/Navbar'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>{children}</body>
+      <body className={openSans.className}>
+        <Navbar />
+        {children}
+        {/*<Footer />*/}
+        </body>
     </html>
   )
 }
