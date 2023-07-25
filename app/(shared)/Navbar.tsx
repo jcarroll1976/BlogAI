def04c0d9e84ba2425274180e80c9ba1200b744c
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-24 20:49:26 -0400

    Added links to navbar

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
 function Navbar({}: Props) {
   return (
     <header className='mb-5'>
-        <nav className='flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4'>nav</nav>
+        <nav className='flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4'>
+            <div className='hidden sm:block'>
+                social link
+            </div>
+            <div className='flex justify-between items-center gap-10'>
+                <Link href="/">Home</Link>
+                <Link href="/">Trending</Link>
+                <Link href="/">About</Link>
+            </div>
+            <div>
+                <p>Sign In</p>
+            </div>
+        </nav>
+        <div className='flex justify-between gap-8 mt-5 mb-4 mx-10'>
+            
+        </div>
     </header>
   )
 }
