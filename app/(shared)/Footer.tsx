6d1038c26a7c22344ab23c3d8fbd9bea621bc744
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-26 22:24:37 -0400

    Finished content for footer

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -8,13 +8,36 @@ function Footer() {
             {/*FIRST COLUMN*/}
             <div className='mt-16 basis-1/2 sm:mt-0'>
                 <h4 className='font-bold uppercase'>Blog of the future</h4>
-                <p>
+                <p className='my-5'>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam atque
                     harum, impedit nihil tenetur obcaecati amet autem adipisci soluta voluptatem 
                     dolorum nobis cupiditate earum reiciendis minima consequatur excepturi qui doloremque.
                 </p>
                 <p>
                     &copy; Blog of the Future All Rights Reserved
+                </p>
+            </div>
+            {/*SECOND COLUMN*/}
+            <div className='mt-16 basis-1/4 sm:mt-0'>
+                <h4 className='font-bold'>Links</h4>
+                <p className='my-5'>
+                    Massa orci senectus
+                </p>
+                <p className='my-5'>
+                    Some random link again
+                </p>
+                <p>
+                    Ullamcorper vivamus
+                </p>
+            </div>
+            {/*THIRD COLUMN*/}
+            <div className='mt-16 basis-1/4 sm:mt-0'>
+                <h4 className='font-bold'>Contact Us</h4>
+                <p className='my-5'>
+                    Tempus metus mattis risus volutpat egestas
+                </p>
+                <p>
+                    (734)555-1212
                 </p>
             </div>
         </div>
