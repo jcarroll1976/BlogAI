7ec4ac998acd01ae51b86ca3a8e44cf91a173390
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-26 22:03:40 -0400

    added content to footer

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -3,7 +3,22 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='bg-wh-900 text-wh-50 py-10 px-10'>Footer</footer>
+    <footer className='bg-wh-900 text-wh-50 py-10 px-10'>
+        <div className='justify-between mx-auto gap-16 sm:flex'>
+            {/*FIRST COLUMN*/}
+            <div className='mt-16 basis-1/2 sm:mt-0'>
+                <h4 className='font-bold uppercase'>Blog of the future</h4>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam atque
+                    harum, impedit nihil tenetur obcaecati amet autem adipisci soluta voluptatem 
+                    dolorum nobis cupiditate earum reiciendis minima consequatur excepturi qui doloremque.
+                </p>
+                <p>
+                    &copy; Blog of the Future All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </footer>
   )
 }
 
