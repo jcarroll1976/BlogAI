e782d2d7e5b8e01839738a68cff3339ea6d6b5e1
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-30 16:32:11 -0400

    completed Card component, added Card layout for tech section

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from 'app/(shared)/Card'
 
 type Props = {}
 
@@ -26,10 +27,21 @@ function Tech({}: Props) {
 
         {/* grid option*/}
         <div className='sm:grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-8 my-5'>
-            <div className='bg-wh-500 col-span-1 row-span-3'></div>
-            <div className='bg-wh-500 col-span-1 row-span-1'></div>
-            <div className='bg-wh-500 col-span-1 row-span-1'></div>
-            <div className='bg-wh-500 col-span-1 row-span-1'></div>
+            {/*LARGE CARD*/}
+            <Card className='bg-wh-500 col-span-1 row-span-3'
+            imageHeight='h-96'
+            isLongForm
+            />
+            {/*SMALL CARDS*/}
+            <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+            imageHeight='h-48'
+            isSmallCard/>
+            <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+            imageHeight='h-48'
+            isSmallCard/>
+            <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+            imageHeight='h-48'
+            isSmallCard/>
         </div>
     </section>
   )
