2c0a05dd61b934ed0a4eb459e0c448bb47d560a8
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-31 20:58:20 -0400

    added  travel component

---
+++ app/(home)/Travel.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Travel({}: Props) {
+  return (
+    <section className="mt-10">
+        <hr className="border-1" />
+    </section>
+  )
+}
+
+export default Travel
