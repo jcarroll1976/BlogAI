c66dbe42c582e7591e8ff5ef503b27e18f8b560f
Joshua Carroll <carrolljosh1976@gmail.com>
2023-07-31 21:25:06 -0400

    started to build out layout for travel section

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from 'app/(shared)/Card'
 
 type Props = {}
 
@@ -6,6 +7,29 @@ function Travel({}: Props) {
   return (
     <section className="mt-10">
         <hr className="border-1" />
+        {/*HEADER*/}
+        <div className='flex items-center gap-3 my-8'>
+            <h4 className='bg-accent-green py-2 px-5 text-wh-900 text-sm font-bold'>
+                TRAVEL
+            </h4>
+            <p className='font-bold text-2xl uppercase'>New Travel Experiences</p>
+        </div>
+
+        {/*CARDS ROW*/}
+        <div className='sm:flex justify-between gap-8'>
+        <Card className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+            imageHeight='h-80'
+            />
+        <Card className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+            imageHeight='h-80'
+        />
+        <Card className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+            imageHeight='h-80'
+        />
+        </div>
+        <Card className='bg-wh-500 sm:flex justify-between items-center gap-3 mt-7 mb-5'
+            imageHeight='h-80'
+        />
     </section>
   )
 }
